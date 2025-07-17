c507995e80862db10d569e662bbf1a5646d495af
OscarRu <oscar.ruizhu@gmail.com>
2025-07-17 16:23:20 -0600

    "Add functional interface examples, including Function, BiFunction, UnaryOperator, BinaryOperator, Consumer, Supplier, Predicate, and BiPredicate implementations."

---
+++ fundamentos/src/main/java/paquete_function/interface_consumer/InterfaceConsumer.java
@@ -0,0 +1,17 @@
+package paquete_function.interface_consumer;
+
+import java.util.*;
+
+public class InterfaceConsumer {
+    public static void main(String[] args) {
+        Set<Integer> nums = Set.of(5,4,3,2,1);
+        SortedSet<Integer> sortedNums = new TreeSet<>(nums);
+        List<Integer> squares = new ArrayList<>();
+
+        sortedNums.forEach(n -> squares.add(n*n));
+        squares.forEach(System.out::println);
+
+        Map<Boolean, String> map = Map.of(true, "Yes", false, "No");
+        map.forEach((k,v) -> System.out.println(k + "-" + v));
+    }
+}
