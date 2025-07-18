280d0dce8f262c6776522f79d3639284db57fc59
OscarRu <oscar.ruizhu@gmail.com>
2025-07-18 11:32:05 -0600

    "Add new Java examples showcasing Strategy Pattern, Comparator, Threads, and File Reading using lambdas."

---
+++ fundamentos/src/main/java/lambda_applications/FileReader.java
@@ -0,0 +1,21 @@
+package lambda_applications;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    public static void main(String[] args) {
+        Path file = Path.of("src/main/resources/lambdas.txt");
+
+        try(Stream<String> lines = Files.lines(file).onClose(() -> System.out.println("Buffer closed"))){
+
+            lines.forEach(System.out::println);
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
---
+++ fundamentos/src/main/java/lambda_applications/ThreadsAndLambdas.java
@@ -0,0 +1,47 @@
+package lambda_applications;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+public class ThreadsAndLambdas {
+
+    static Runnable printerSum = ()-> {
+            long result = 0;
+            System.out.println(Thread.currentThread().getName());
+            for (int i = 0; i < 10000000; i++) {
+                result += i;
+            }
+            System.out.println("Total " + result);
+    };
+
+    static Callable<Long> getSum = () -> {
+        long result = 0;
+        for (int i = 0; i < 10000000; i++) {
+            result += i;
+        }
+        return result;
+    };
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        System.out.println(Thread.currentThread().getName());
+
+        var exec = Executors.newSingleThreadExecutor();
+
+        exec.submit(printerSum);
+
+        var result = exec.submit(getSum);
+        try {
+            System.out.println("Result: " + result.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+        exec.shutdown();
+
+
+    }
+}
