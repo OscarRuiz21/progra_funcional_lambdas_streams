05c083dd63b790436b0e90b7d95dda043f99d07a
OscarRu <oscar.ruizhu@gmail.com>
2025-07-21 14:46:49 -0600

    "Add new Java examples showcasing Strategy Pattern, Comparator, Threads, and File Reading using lambdas."

---
+++ fundamentos/src/main/java/streams/ConstructorStreams.java
@@ -0,0 +1,33 @@
+package streams;
+
+import org.w3c.dom.ls.LSOutput;
+import util.Database;
+import util.Videogame;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+public class ConstructorStreams {
+
+    public static void main(String[] args) {
+        Stream<Videogame> myFirstStream = Database.videogames.stream();
+        myFirstStream.forEach(System.out::println);
+
+        System.out.println("-------------------------------------");
+
+        Integer[] myArray = {1,2,3,4,5};
+        Stream<Integer> mySecondStream = Stream.of(myArray);
+        mySecondStream.forEach(System.out::println);
+
+        System.out.println("-------------------------------------");
+
+        Stream.of("Hello", "World").forEach(System.out::println);
+
+        System.out.println("-------------------------------------");
+
+        DoubleStream myDoubleStream = DoubleStream.of(1.0, 2.0, 3.0, 4.0, 5.0);
+        myDoubleStream.forEach(System.out::println);
+
+    }
+
+}
